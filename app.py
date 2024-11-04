016bd7df8c2ebe9f8dbc4dafd09ead101ec1c453
Thirulok Ranganathan <thirulokranganathan@gmail.com>
2024-11-04 13:26:28 +0530

    Improved code and fixed some errors

--- app.py
+++ app.py
@@ -4,44 +4,55 @@ import json
 import time
 
 class KeyValueStore:
-    def __init__(self, file_path = "data/data_store.json"):
+    def __init__(self, file_path = "data_store.json"):
         self.file_path = file_path
         self.lock = threading.Lock()
         self.data = {}
         self.load_dataStore()
 
     def load_dataStore(self):
-        if os.path.exists(self.file_path):
-            with open(self.file_path, 'r') as read:
-                self.data = json.load(read)
-            read.close()
-            return self.data
-        else:
-            return "No keys added in Data Store"
+        with self.lock:
+            if os.path.exists(self.file_path):
+                with open(self.file_path, 'r') as read:
+                    self.data = json.load(read)
+                read.close()
+                return self.data
+            else:
+                return "No keys are added in Data Store."
         
     def save_dataStore(self):
-        with open(self.file_path, 'w') as write:
-            json.dump(self.data, write)
-        write.close()
+        with self.lock:
+            with open(self.file_path, 'w') as write:
+                json.dump(self.data, write)
+            write.close()
 
     def create(self, key, value, ttl=None):
-        if self.data:
-            if key in self.data:
-                return "key already exist in the Data Store"
+        with self.lock:
+            if self.data:
+                if key in self.data:
+                    return f"Error: Key '{key}' already exist in the Data Store."
+                
+            if not isinstance(key, str):
+                return "Error: key must be in string format."
+            elif not isinstance(value, dict):
+                return "Error: Value must be in json format."
+            elif ttl is not None and not isinstance(ttl, int):
+                return "Error: TTL must be an integer."
+            else:
+                if len(key) > 32:
+                        return f"Error: Key '{key}' length should not exceed the limit of 32 characters."
+                
+                expiry = time.time() + ttl if ttl else None
 
-        if len(key) > 32:
-                return "Key length should not exceed the limit of 32 characters"
-        
-        expiry = time.time() + ttl if ttl else None
-
-        self.data[key] = {"value": value, 'expiry': expiry}
-        self.save_dataStore()
+                self.data[key] = {"value": value, 'expiry': expiry}
+                self.save_dataStore()
+                return "Key value pair added Successfully."
 
     def read(self, key):
         with self.lock:
             content = self.data.get(key)
             if not content:
-                return "Key not Found in Data Store"
+                return "Key not Found in Data Store \n Check if the key is correct and does not exceed 32 characters."
             elif content['expiry'] and time.time() > content['expiry']:
                 del self.data[key]
                 self.save_dataStore()
@@ -50,6 +61,8 @@ class KeyValueStore:
                 return content['value']
             
     def delete(self, key):
+        if len(key) > 32:
+            return "Error: Key exceeds 32 characters. \n check if the key is correct."
         with self.lock:
             if key in self.data:
                 del self.data[key]
@@ -59,21 +72,33 @@ class KeyValueStore:
                 return "key not found in Data Store"
 
     def batch_create(self, keys):
-        for object in keys:
-            if len(object) == 3:
-                key, value, ttl = object
-            elif len(object) == 2:
-                key, value, ttl = object[0], object[1], None
-            else:
-                return "Error: Provide a valid format containing (key,value) or (key,value,ttl). Note: the value is of type dictionary."
-            self.create(key, value, ttl)
+        if len(keys) > 50:
+            return "Error: Batch create exceeds the limit of 50"
+
+        errors = []
+        with self.lock:
+            for object in keys:
+                if len(object) == 3:
+                    key, value, ttl = object
+                elif len(object) == 2:
+                    key, value, ttl = object[0], object[1], None
+                else:
+                    return "Error: Provide a valid format containing (key,value) or (key,value,ttl). Note: the value is of type dictionary."
+                
+                if not isinstance(key, str):
+                    errors.append(f"Error: key '{key}' must be in string format.") 
+                    continue
+                elif not isinstance(value, dict):
+                    errors.append(f"Error: Value for key '{key}' must be a json object.")
+                    continue
+                elif ttl is not None and not isinstance(ttl, int):
+                    errors.append(f"Error: TTL for key '{key}' must be an integer.")
+                    continue
+
+                result = self.create(key, value, ttl)
+                if "Error" in result:
+                    errors.append(result)
+
+        if errors:
+            return "\n".join(errors)
         return "key-value pairs added successfully"
-
-
-print(KeyValueStore().batch_create([("erg", {"value": "valve"}),("dhdhergew", {"value": "dome"}),("3sdfdsaf", {"value": "hash"})]))
-# print(KeyValueStore().delete('fewqqgfwegw'))
-# print(KeyValueStore().read('fewqqgfwegw'))
-# print(time.time())
-# print (KeyValueStore().load_dataStore())
-# KeyValueStore().create("weggvasdvwbghw", {"value": "amazon"}, ttl=22)
-# KeyValueStore().create("fewqqgfwegw", {"value": "google"})
